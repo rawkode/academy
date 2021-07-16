77a69bf69cd1c37b5cb5ad9d6161b572bdfac269
David McKay <david@rawkode.com>
2021-07-16 14:27:40 +0100

    chore: remove netlify

--- websites/api.rawkode.academy/.netlify/functions-serve/hello/hello.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./src/hello.js')
--- websites/api.rawkode.academy/.netlify/functions-serve/hello/src/hello.js
+++
@@ -1,8 +0,0 @@
-// functions/hello.js
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
-};
-//# sourceMappingURL=hello.js.map
