920ecc5d85f41ba8eade46ea4eecd481e7c93dab
David McKay <david@rawkode.com>
2021-07-16 14:32:02 +0100

    chore: switch to academy data

--- websites/api.rawkode.academy/api/index.js
+++ websites/api.rawkode.academy/api/index.js
@@ -5,7 +5,7 @@ const fetch = require("sync-fetch");
 import jsonGraphqlExpress from "json-graphql-server";
 
 const data = fetch(
-  "https://github.com/rawkode/rawkode/releases/download/blox/data.json"
+  "https://github.com/rawkode/academy/releases/download/blox/data.json"
 ).json();
 
 const app = require("express")();
